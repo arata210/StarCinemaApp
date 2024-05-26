669015a8d9e41727660194cd7e7089790b54a991
Xroo <sl20113419@gmail.com>
2024-05-26 15:22:19 +0800

    Revert "update ver1.2"
    This reverts commit 3a57c22ef9ce802a75daad4ce058e5b473ca2084.

---
+++ app/src/main/java/moe/arata210/starcinema/UserAgreement.java
@@ -0,0 +1,14 @@
+package moe.arata210.starcinema;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserAgreement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_agreement);
+    }
+}
